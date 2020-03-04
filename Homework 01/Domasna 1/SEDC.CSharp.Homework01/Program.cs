404a12b43a3241a93b1a73a5e8171b2f3a8dc5ea
Daniel Petkovski <daniel.petkovski@live.comgit config --global user.name Daniel>
2020-03-04 12:40:03 +0100

    Full Homework

---
+++ Homework 01/Domasna 1/SEDC.CSharp.Homework01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+namespace SEDC.CSharp.Homework01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Traffic Light....\n");
+            Thread.Sleep(2000);
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("Red Light - STOP!\n");
+            Thread.Sleep(2000);
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine("Yellow Light - Prepare.\n");
+            Thread.Sleep(2000);
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("Green Light - Go!\n");
+
+            Console.ResetColor();
+            Console.ReadLine();
+        }
+    }
+}
